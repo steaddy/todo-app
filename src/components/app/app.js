5c2355ce893a9cd2012a82e41b50c5902de9b09a
Kirichenko_Gennadii <9254497089@mail.ru>
2020-11-29 00:47:46 +0300

    Add few components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import TaskList from '../task-list';
+import NewTaskForm from '../new-task-form';
 import './app.css';
 
 export default class App extends Component {
@@ -8,45 +10,10 @@ export default class App extends Component {
             <section className="todoapp">
                 <header className="header">
                     <h1>todos</h1>
-                    <input className="new-todo" placeholder="What needs to be done?" autofocus/>
+                    <NewTaskForm/>
                 </header>
                 <section className="main">
-                    <ul className="todo-list">
-                        <li className="completed">
-                            <div className="view">
-                                <input className="toggle" type="checkbox"/>
-                                    <label>
-                                        <span className="description">Completed task</span>
-                                        <span className="created">created 17 seconds ago</span>
-                                    </label>
-                                    <button className="icon icon-edit"></button>
-                                    <button className="icon icon-destroy"></button>
-                            </div>
-                        </li>
-                        <li className="editing">
-                            <div className="view">
-                                <input className="toggle" type="checkbox"/>
-                                    <label>
-                                        <span className="description">Editing task</span>
-                                        <span className="created">created 5 minutes ago</span>
-                                    </label>
-                                    <button className="icon icon-edit"></button>
-                                    <button className="icon icon-destroy"></button>
-                            </div>
-                            <input type="text" className="edit" value="Editing task"/>
-                        </li>
-                        <li>
-                            <div className="view">
-                                <input className="toggle" type="checkbox"/>
-                                <label>
-                                    <span className="description">Active task</span>
-                                    <span className="created">created 5 minutes ago</span>
-                                </label>
-                                <button className="icon icon-edit"></button>
-                                <button className="icon icon-destroy"></button>
-                            </div>
-                        </li>
-                    </ul>
+                    <TaskList/>
                     <footer className="footer">
                         <span className="todo-count">1 items left</span>
                         <ul className="filters">
